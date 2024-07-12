927c59cf61b64c4df1f088b4120c950f88166764
Maojin-juan <edwardelric199887@gmail.com>
2024-07-12 18:33:17 +0800

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        bruno: ['Bruno Ace SC', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
